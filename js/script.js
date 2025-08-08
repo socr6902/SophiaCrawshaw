66c51042a0733fb4c7a41e19195145daa939ea5e
socr6902 <socr6902@colorado.edu>
2025-08-08 14:18:32 -0400

    image carosel

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.accordion').forEach(button => {
+    button.addEventListener('click', () => {
+      button.classList.toggle('active');
+      const panel = button.nextElementSibling;
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+      } else {
+        panel.style.display = 'block';
+      }
+    });
+  });
+  
