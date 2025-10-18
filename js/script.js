699c44e1e4835facd73958ce3c9739b1e156e5ef
socr6902 <socr6902@colorado.edu>
2025-10-18 15:54:00 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -10,19 +10,3 @@ document.querySelectorAll('.accordion').forEach(button => {
     });
   });
 
-
-  document.querySelectorAll('.flip-card__inner').forEach(btn => {
-    btn.addEventListener('click', () => {
-      btn.classList.toggle('is-flipped');
-      const expanded = btn.classList.contains('is-flipped');
-      btn.setAttribute('aria-expanded', expanded ? 'true' : 'false');
-    });
-
-    btn.addEventListener('keydown', e => {
-      if (e.key === 'Enter' || e.key === ' ') {
-        e.preventDefault();
-        btn.click();
-      }
-    });
-  });
-
